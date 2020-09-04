6fe02502c418b178af5ed2d1d41dcf2741d2f01a
cttd08 <throw@cttd.co.kr>
2020-09-04 16:14:01 +0900

    VuePress 기본 설정

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git push -f https://github.com/ChoiByungNam/vuepress-blog.git master:gh-pages
+git push -f https://github.com/ChoiByungNam/vuepress-blog.git master
 
 cd -
